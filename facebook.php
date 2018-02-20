4be88c357d583a71bb802ed19efe41a5a8d89d04
Thaniia <34150927+Thaniia@users.noreply.github.com>
2018-02-20 17:06:03 +0800

    Add files via upload

---
+++ facebook.php
@@ -0,0 +1,29 @@
+<?php
+ 
+$access_token = "Ganti Sama Tokenmu";
+$url = "https://graph.facebook.com/me/feed?method=POST";
+
+if(!empty($_GET['x'])){
+$status = $_GET['x'];
+} else {
+
+$send = file("http://www.sbsseh.com/fb/mantan.php");
+$icha = $send[array_rand($send)];
+$statusku= $icha; 
+
+} 
+
+    $ch = curl_init();
+    $attachment =  array(   'access_token'  => $access_token,                       
+                        'message'          => $statusku,
+                    );
+
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $attachment);
+    $result= curl_exec($ch);
+
+    curl_close ($ch);
+?>
